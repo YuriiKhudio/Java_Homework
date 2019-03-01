d06dfd5b3167c5771f01e3f16bedbc62e8d89220
YuriiKhudio <38938810+YuriiKhudio@users.noreply.github.com>
2019-03-01 17:25:06 +0200

    lesson1

---
+++ Task2/task_2.java
@@ -0,0 +1,34 @@
+package Task2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+
+public class task_2 {
+
+    public static void main(String[] args) {
+        int[] array = {4,8,16,38,5,42,4,5,34,78,41,4,38,87,3}; // created random array for example
+
+        //конвертнув арей в арей ліст
+        ArrayList<Integer> myList = new ArrayList<>();
+        for (int i = 0; i < array.length; i++){
+            myList.add(array[i]);
+        }
+
+        //перевернув ліст
+        Collections.reverse(myList);
+
+        // прінтанув його
+        for (int i : myList) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        //тут через лінкед хаш сет забрав всі дублікати і вивів в консоль зі збереженням послідовності
+        myList = new ArrayList<Integer>(new LinkedHashSet<Integer>(myList));
+        for (int i : myList) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
